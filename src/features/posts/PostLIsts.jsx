c5c41737bfca9bd565992b74da26104c92bf6f8f
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2024-12-27 16:47:48 +0900

    added TimeAgo

--- src/features/posts/PostLIsts.jsx
+++ src/features/posts/PostLIsts.jsx
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux'
 import { selectAllPosts } from './postsSlice'
 import PostAuthor from './PostAuthor'
-import React from 'react'
+import TimeAgo from './TimeAgo'
+
 
 const PostLIsts = () => {
   
@@ -13,7 +14,7 @@ const PostLIsts = () => {
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
           <PostAuthor userId={post.userId} />
-    
+          <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ))
--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -1,8 +1,17 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
-    {id: '1', title: 'Learning redux toolkit', content: "i've heard a good things."},
-    {id: '2', title: 'Slices...', content: 'The more i say slice, the more i want pizza.' }
+    {id: '1', 
+    title: 'Learning redux toolkit', 
+    content: "i've heard a good things.",
+    date: sub(new Date(), {minutes: 10}).toISOString()
+},
+    {id: '2', 
+    title: 'Slices...', 
+    content: 'The more i say slice, the more i want pizza.', 
+    date: sub(new Date(), {minutes: 5}).toISOString()
+}
 ]
 
 const postsSlice = createSlice({
@@ -19,6 +28,7 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
+                        date: new Date().toISOString,
                         userId
                         
                     }
