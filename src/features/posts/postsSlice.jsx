69fff224acce6976b22505b28ddfd40e11d0b340
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2024-12-27 17:24:47 +0900

    TimeAgo working

--- src/features/posts/postsSlice.jsx
+++ src/features/posts/postsSlice.jsx
@@ -28,7 +28,7 @@ const postsSlice = createSlice({
                         id: nanoid(),
                         title,
                         content,
-                        date: new Date().toISOString,
+                        date: new Date().toISOString(),
                         userId
                         
                     }
