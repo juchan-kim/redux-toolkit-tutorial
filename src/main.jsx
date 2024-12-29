55e9a5617f7037f58702af43fafd96378514c5b9
juchan-kim <113649125+juchan-kim@users.noreply.github.com>
2024-12-29 16:19:21 +0900

    finished middleware and thunk

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './app/store.jsx'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice.jsx'
+
+store.dispatch(fetchUsers())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
